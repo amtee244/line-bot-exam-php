cb0528a028e69827b027355390f9d36451909263
Amtee244 <teerayut17100@hotmail.com>
2019-08-07 20:44:12 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'vdDDbMJrLQHhYxmAWhG9czicHhR1dbHi9mJlTrgoyy7LhWNnQEb6IjoM1RqSEh+qayZNJdHDcBmNuISJtEKFED6JZ6ERFiVWNttbTCRfDxoHbDzpprOQsXPbmk55fdofx0L2YcBBOhJ15KT1l5db1AdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
