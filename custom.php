8ca7893078d5e28c998a652eb5d2dad72274ca53
Amtee244 <teerayut17100@hotmail.com>
2019-08-13 14:46:13 +0700

    Update custom.php

--- custom.php
+++ custom.php
@@ -17,11 +17,10 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
-			$messages = ['				
-   				"type": "sticker",
-    				"packageId": "11537",
-    				"stickerId": "52002735"
-			'];
+			$messages = [				
+   				'type': 'sticker',
+    				'packageId': '11537',
+    				'stickerId': '52002735'];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
