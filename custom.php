7d0850d7198f8b713634a8d53f31a85ed9451fc0
Amtee244 <teerayut17100@hotmail.com>
2019-08-13 13:07:08 +0700

    Update custom.php

--- custom.php
+++ custom.php
@@ -18,8 +18,184 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text
+				'
+    "type": "flex",
+    "altText": "Flex Message",
+    "contents": {
+      "type": "carousel",
+      "contents": [
+        {
+          "type": "bubble",
+          "hero": {
+            "type": "image",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
+            "size": "full",
+            "aspectRatio": "20:13",
+            "aspectMode": "cover"
+          },
+          "body": {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "text",
+                "text": "Arm Chair, White",
+                "size": "sm",
+                "weight": "bold",
+                "wrap": true
+              },
+              {
+                "type": "box",
+                "layout": "baseline",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "$49",
+                    "flex": 0,
+                    "size": "sm"
+                  },
+                  {
+                    "type": "text",
+                    "text": ".99",
+                    "flex": 0,
+                    "size": "sm",
+                    "weight": "bold",
+                    "wrap": true
+                  }
+                ]
+              }
+            ]
+          },
+          "footer": {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "button",
+                "action": {
+                  "type": "uri",
+                  "label": "Add to Cart",
+                  "uri": "https://linecorp.com"
+                },
+                "style": "primary"
+              },
+              {
+                "type": "button",
+                "action": {
+                  "type": "uri",
+                  "label": "Add to whishlist",
+                  "uri": "https://linecorp.com"
+                }
+              }
+            ]
+          }
+        },
+        {
+          "type": "bubble",
+          "hero": {
+            "type": "image",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png",
+            "size": "full",
+            "aspectRatio": "20:13",
+            "aspectMode": "cover"
+          },
+          "body": {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "text",
+                "text": "Metal Desk Lamp",
+                "size": "xl",
+                "weight": "bold",
+                "wrap": true
+              },
+              {
+                "type": "box",
+                "layout": "baseline",
+                "flex": 1,
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "$11",
+                    "flex": 0,
+                    "size": "xl",
+                    "weight": "bold",
+                    "wrap": true
+                  },
+                  {
+                    "type": "text",
+                    "text": ".99",
+                    "flex": 0,
+                    "size": "sm",
+                    "weight": "bold",
+                    "wrap": true
+                  }
+                ]
+              },
+              {
+                "type": "text",
+                "text": "Temporarily out of stock",
+                "flex": 0,
+                "margin": "md",
+                "size": "xxs",
+                "color": "#FF5551",
+                "wrap": true
+              }
+            ]
+          },
+          "footer": {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "button",
+                "action": {
+                  "type": "uri",
+                  "label": "Add to Cart",
+                  "uri": "https://linecorp.com"
+                },
+                "flex": 2,
+                "color": "#AAAAAA",
+                "style": "primary"
+              },
+              {
+                "type": "button",
+                "action": {
+                  "type": "uri",
+                  "label": "Add to wish list",
+                  "uri": "https://linecorp.com"
+                }
+              }
+            ]
+          }
+        },
+        {
+          "type": "bubble",
+          "body": {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "button",
+                "action": {
+                  "type": "uri",
+                  "label": "See more",
+                  "uri": "https://linecorp.com"
+                },
+                "flex": 1,
+                "gravity": "center"
+              }
+            ]
+          }
+        }
+      ]
+    }'
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -28,26 +204,26 @@ if (!is_null($events['events'])) {
 				'messages' => [$messages],
 			];
 			
-			
-$servername = "61.19.71.179";
-$username = "sso";
-$password = "075271283";
-$dbname = "line";
+/*			
+//$servername = "61.19.71.179";
+//$username = "sso";
+//$password = "075271283";
+//$dbname = "line";
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+//$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-$sql = "INSERT INTO profile (UID, name)
-VALUES ('".$text."', 'Doe')";
+//if ($conn->connect_error) {
+ //   die("Connection failed: " . $conn->connect_error);
+//} 
+//$sql = "INSERT INTO profile (UID, name)
+//VALUES ('".$text."', 'Doe')";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 $conn->close();
-			
+		*/	
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 			$ch = curl_init($url);
